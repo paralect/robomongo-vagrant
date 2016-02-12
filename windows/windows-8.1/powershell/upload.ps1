72dc892250c767b976fef33e015a903994261a84
anorsich <andrew@paralect.com>
2016-02-12 16:05:02 +0300

    [*] Bump version to rc6

--- windows/windows-8.1/powershell/upload.ps1
+++ windows/windows-8.1/powershell/upload.ps1
@@ -1,3 +1,3 @@
 cd $args[0]
 $robomongoDist = (Get-ChildItem *.exe | % { $_.FullName })
-curl.exe -i -X POST -H "Content-Type: multipart/form-data" -F "image=@$robomongoDist" $args[1]
+curl.exe --insecure -i -X POST -H "Content-Type: multipart/form-data" -F "image=@$robomongoDist" $args[1]
