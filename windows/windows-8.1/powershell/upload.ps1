50981231401be4de6d8820116a7a760c7655b1aa
anorsich <andrew@paralect.com>
2016-02-13 15:14:07 +0300

    [+] Upload zip archive for windows

--- windows/windows-8.1/powershell/upload.ps1
+++ windows/windows-8.1/powershell/upload.ps1
@@ -1,3 +1,6 @@
 cd $args[0]
 $robomongoDist = (Get-ChildItem *.exe | % { $_.FullName })
 curl.exe --insecure -i -X POST -H "Content-Type: multipart/form-data" -F "image=@$robomongoDist" $args[1]
+
+$robomongoDist = (Get-ChildItem *.zip | % { $_.FullName })
+curl.exe --insecure -i -X POST -H "Content-Type: multipart/form-data" -F "image=@$robomongoDist" $args[1]
